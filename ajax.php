efdc9ff829f40321e9bac8afa8324f8bf0ad2fa6
sebastianjof <sebastianjof@gmail.com>
2022-06-18 16:23:34 -0300

    1

---
+++ ajax.php
@@ -0,0 +1,216 @@
+<?php
+
+if (!defined('NOCSRFCHECK')) {
+	define('NOCSRFCHECK', '1');
+}
+if (!defined('NOTOKENRENEWAL')) {
+	define('NOTOKENRENEWAL', '1');
+}
+if (!defined('NOREQUIREMENU')) {
+	define('NOREQUIREMENU', '1');
+}
+if (!defined('NOREQUIREHTML')) {
+	define('NOREQUIREHTML', '1');
+}
+if (!defined('NOREQUIREAJAX')) {
+	define('NOREQUIREAJAX', '1');
+}
+if (!defined('NOBROWSERNOTIF')) {
+	define('NOBROWSERNOTIF', '1');
+}
+
+require '../../main.inc.php'; 
+// load college libraries
+require_once __DIR__.'/class/notes.class.php';
+require_once __DIR__.'/class/classrooms.class.php';
+require_once __DIR__.'/class/student.class.php';
+require_once __DIR__.'/class/inscriptions.class.php';
+require_once __DIR__.'/class/subject.class.php';
+
+
+global $db, $user, $langs; 
+
+$action = GETPOST('action', 'aZ09');
+$idClass = GETPOST('idClass', 'int');
+$idSubjejct = GETPOST('idSubjejct', 'int');
+
+$arralumnoperiodo = GETPOST('arralumnoperiodo', 'intcomma');
+$notavalue = GETPOST('notavalue', 'aZ09');
+$idNota = GETPOST('idNota', 'int');
+
+$estudiante = new Student($db);
+
+if($action == 'getsubject' && !empty($idClass) ){
+//SELECT rowid, label, fk_class FROM llx_college_subject WHERE fk_class = 2 - WHERE fk_class = ".(int)$idClass."  , JSON_UNESCAPED_UNICODE
+ $rows = array();
+ $resql=$db->query("SELECT rowid, label FROM ".MAIN_DB_PREFIX."college_subject WHERE fk_class = ".(int)$idClass." ");
+  if ($resql) {
+      $num = $db->num_rows($resql);
+         $i = 0;
+         if ($num)
+         {
+           while ($i < $num)
+           {
+             $obj = $db->fetch_object($resql);
+             if ($obj)
+             {
+              $rows[] = array('id' => $obj->rowid,'text' => $obj->label);
+             }
+             $i++;
+           }
+         }
+      echo json_encode($rows);
+      exit;
+  }else{
+      echo json_encode(0);
+      exit;
+  }
+}
+
+if($action == 'getstudent' && !empty($idClass) ){
+  $rows = array();
+  $resql = $db->query( "SELECT i.fk_student, s.label 
+  FROM ".MAIN_DB_PREFIX."college_inscriptions AS i 
+  INNER JOIN ".MAIN_DB_PREFIX."college_student AS s ON i.fk_student=s.rowid 
+  WHERE i.fk_class =  ".(int)$idClass."  AND i.`status` = 1 AND i.school_year = ".$conf->global->COLLEGE_MYPARAM_CICLO_LECTIVO." ORDER BY s.label ASC ");
+ 
+  if ($resql) {
+      $num = $db->num_rows($resql);
+         $i = 0;
+         if ($num)
+         {
+           while ($i < $num)
+           {
+             $obj = $db->fetch_object($resql);
+             if ($obj)
+             {
+              $rows[] = array(
+                'fk_student' => $obj->fk_student,
+                'label' => $obj->label
+              );
+             }
+             $i++;
+           }
+         }
+      echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+      exit;
+  }else{
+      echo json_encode(0);
+      exit;
+  }
+}
+
+if($action == 'getperiods'){
+  $rows = array();
+  $resql = $db->query("SELECT rowid, label FROM ".MAIN_DB_PREFIX."college_periods WHERE `status` = 1  ");
+    if ($resql) {
+      $num = $db->num_rows($resql);
+         $i = 0;
+         if ($num)
+         {
+           while ($i < $num)
+           {
+             $obj = $db->fetch_object($resql);
+             if ($obj)
+             {
+              $rows[] = array(
+                'rowid' => $obj->rowid,
+                'label' => $obj->label
+              );
+             }
+             $i++;
+           }
+         }
+      echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+      exit;
+  }else{
+      echo json_encode(0);
+      exit;
+  }
+  
+}
+
+
+if($action == 'getnotes' && !empty($idClass)  && !empty($idSubjejct) ){
+  $rows = array();
+  $sql = $db->query("SELECT rowid, fk_student, trimestre, nota FROM llx_college_notes WHERE fk_class = ".(int)$idClass." AND fk_subject = ".(int)$idSubjejct." ");
+  if ($sql) {
+  $num = $db->num_rows($sql);
+     $i = 0;
+     if ($num)
+     {
+       while ($i < $num)
+       {
+         $obj = $db->fetch_object($sql);
+         if ($obj)
+         {
+          $rows[] = array(
+            'rowid'=> (int)$obj->rowid,
+            'data' => array((int)$obj->fk_student, (int)$obj->trimestre),
+            'nota' => (float)$obj->nota
+          );
+         }
+         $i++;
+       }
+     }
+  echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+  exit;
+  }else{
+      echo json_encode(0);
+      exit;
+  }
+
+}
+
+
+
+if($action == 'senddata' && !empty($idClass)  && $idSubjejct!= -1  && !empty($arralumnoperiodo)  && !empty($notavalue)  ){
+  
+  $data = explode(',',$arralumnoperiodo);
+  
+    /*NINJA*/
+    $query = $db->query("SELECT rowid FROM ".MAIN_DB_PREFIX."college_notes WHERE fk_subject = ".(int)$idSubjejct." AND fk_student = ".(int)$data[0]." AND trimestre = ".(int)$data[1]."  ");
+    $obj = $db->fetch_object($query);
+  if($query){
+      
+    $db->begin();
+    $sql = "INSERT INTO ".MAIN_DB_PREFIX."college_notes";
+    $sql .="(rowid ,fk_user_creat,school_year,status,fk_class,fk_user,fk_subject,trimestre,fk_student,nota)";
+    $sql .=" VALUES(".(int)$obj->rowid.", ".$user->id.", ".$conf->global->COLLEGE_MYPARAM_CICLO_LECTIVO.",1,".(int)$idClass.",".(int)$user->id.",".(int)$idSubjejct.",".(int)$data[1].",".(int)$data[0].",".(float)$notavalue.")";
+    $sql .=" ON DUPLICATE KEY UPDATE nota=".(float)$notavalue." ";
+    $resql = $db->query($sql);
+    if($resql){
+      $db->commit();
+      echo json_encode(1);
+      exit;
+    }else{
+      $db->rollback();
+      echo json_encode(0);
+      dol_print_error($db);
+      exit;
+    }
+  
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sql/llx_college_subject.key.sql
@@ -0,0 +1,31 @@
+-- Copyright (C) ---Put here your own copyright and developer email---
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see https://www.gnu.org/licenses/.
+
+
+-- BEGIN MODULEBUILDER INDEXES
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_rowid (rowid);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_ref (ref);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_fk_soc (fk_soc);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_fk_project (fk_project);
+ALTER TABLE llx_college_subject ADD CONSTRAINT llx_college_subject_fk_user_creat FOREIGN KEY (fk_user_creat) REFERENCES llx_user(rowid);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_status (status);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_fk_class (fk_class);
+ALTER TABLE llx_college_subject ADD INDEX idx_college_subject_fk_user (fk_user);
+-- END MODULEBUILDER INDEXES
+
+--ALTER TABLE llx_college_subject ADD UNIQUE INDEX uk_college_subject_fieldxy(fieldx, fieldy);
+
+--ALTER TABLE llx_college_subject ADD CONSTRAINT llx_college_subject_fk_field FOREIGN KEY (fk_field) REFERENCES llx_college_myotherobject(rowid);
+
